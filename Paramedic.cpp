dfcabc7b4af5807da65079f4c9bee86097bbf043
shiraZadok <58064644+shiraZadok@users.noreply.github.com>
2020-06-08 13:15:06 +0300

    Update Paramedic.cpp

--- Paramedic.cpp
+++ Paramedic.cpp
@@ -5,10 +5,8 @@
 
 void Paramedic::attack(std::vector<std::vector<Soldier*>>& board,std::pair<int,int> source){
     std::pair<int,int> attack;
-    //First check where is the closest enemy soldier
     attack = Soldier::SearchEnemyToAttack(board,source,Filter::Close);
     if(attack.first == std::numeric_limits<int>::max()) return;
     if (board[attack.first][attack.second]== nullptr)return;
-    //Attack
     board[attack.first][attack.second]->health() = board[attack.first][attack.second]->maxLife() ;
 }
